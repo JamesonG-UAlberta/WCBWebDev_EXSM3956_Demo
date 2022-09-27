0ca27cc8a6848c712cf0b3d2f428c654ac2a0983
James Grieve <jgrieve@ualberta.ca>
2022-09-27 09:38:37 -0600

    Navigation setup;

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer, StackActions } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <NavigationContainer>
       <Text>Hello, World!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <Stack.Navigator>
+          <Stack.Screen name="" component={} options={} />
+          <Stack.Screen name="" component={} options={} />
+        </Stack.Navigator>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
